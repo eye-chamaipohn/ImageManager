8f44ab54d743dcd3114bb790dce9cd674423e5e2
eye-chamaipohn <Beck@DESKTOP-LL6AVQQ>
2022-09-21 09:05:47 +0900

    Add ImageWpfToGDI method

--- ImageManager/ImageConverter.cs
+++ ImageManager/ImageConverter.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace ImageManager
 {
-    internal class ImageConverter
+    class ImageConverter
     {
+        private System.Drawing.Image ImageWpfToGDI(System.Windows.Media.ImageSource image)
+        {
+            MemoryStream ms = new MemoryStream();
+            var encoder = new BmpBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(image as System.Windows.Media.Imaging.BitmapSource));
+            encoder.Save(ms);
+            ms.Flush();
 
+            return System.Drawing.Image.FromStream(ms);
+        }
     }
 }
