0de8f9476429b46ac4dbf19e62fafa7ec7523d98
eye-chamaipohn <Beck@DESKTOP-LL6AVQQ>
2022-09-21 09:12:28 +0900

    Refactor merhod

--- ImageManager/ImageConverter.cs
+++ ImageManager/ImageConverter.cs
@@ -4,21 +4,22 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Media.Imaging;
 
 namespace ImageManager
 {
     class ImageConverter
     {
-        private System.Drawing.Image ImageWpfToGDI(System.Windows.Media.ImageSource image)
+        private Image ImageWpfToGDI(System.Windows.Media.ImageSource image)
         {
             MemoryStream ms = new MemoryStream();
             var encoder = new BmpBitmapEncoder();
-            encoder.Frames.Add(BitmapFrame.Create(image as System.Windows.Media.Imaging.BitmapSource));
+            encoder.Frames.Add(BitmapFrame.Create(image as BitmapSource));
             encoder.Save(ms);
             ms.Flush();
 
-            return System.Drawing.Image.FromStream(ms);
+            return Image.FromStream(ms);
         }
     }
 }
